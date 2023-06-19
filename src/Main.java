086807146ed8020a6f3c118bbed0f6489255c4ad
79998 <lekha-fadeev@mail.ru>
2023-06-19 15:57:40 +0300

    коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Transport[] transports = new Transport[]{
+                new Car("car1", 4),
+                new Car("car2", 4),
+                new Car("Truck1", 6),
+                new Car("Truck2", 8),
+                new Car("Bicycle1", 2),
+                new Car("Bicycle2", 2)
+        };
+
+        ServiceStation station = new ServiceStation();
+
+    for (Transport transport : transports) {
+        station.check(transport);
+    }
     }
 }
---
+++ src/Transport.java
@@ -0,0 +1,31 @@
+public abstract class Transport implements Serviseable {
+
+    private final String modelName;
+    private final int wheelsCount;
+
+    public Transport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transport{" +
+                "modelName='" + modelName + '\'' +
+                ", wheelsCount=" + wheelsCount +
+                '}';
+    }
+
+    @Override
+    public void doService() {
+        System.out.println("Обслуживаем " + this.getModelName());
+    }
+}
